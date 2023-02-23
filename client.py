521cb331e6379e77c4c83b144e67f8273c549be1
Aro-M <ar.militosyan.97@mail.ru>
2023-02-23 13:47:18 +0400

    add_client

--- client.py
+++ client.py
@@ -1,6 +1,8 @@
 import socket
 import  threading
 
+nickname = input("Choose a nickname  ")
+
 c_host = '127.0.0.1'
 c_port = 55555
 
@@ -11,11 +13,29 @@ def receive():
     while True:
         try:
             message = client.recv(1024).decode('ascii')
-            if message == "NICK":
-                pass
+            if nickname == "NICK":
+                client.send(nickname.encode('ascii'))
             else:
                 print(message)
         except:
             print("Error occurred")
             client.close()
             break
+
+
+def write():
+    while True:
+        message = f'{ nickname }{input("")}'
+        client.send(message.encode('ascii'))
+
+
+
+
+
+
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
+
