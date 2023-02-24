641713b52125aa41842602e2ace61418d6683835
Aro-M <ar.militosyan.97@mail.ru>
2023-02-24 19:23:37 +0400

    changed___.gitignore

--- exceptions.py
+++ exceptions.py
@@ -1,3 +1,4 @@
+
 class SocketException(Exception):
     pass
 
--- settings.py
+++ settings.py
@@ -1,8 +1,8 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv('.env')
 
+load_dotenv('.env')
 ENCODING = os.environ['ENCODING']
 SERVER_ADDRESS = (os.environ['SERVER_HOST'], int(os.environ['SERVER_PORT']))
 ENCRYPTION_KEY = int(os.environ['ENCRYPTION_KEY'])
